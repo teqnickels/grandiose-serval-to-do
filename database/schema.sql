f96434d300f1a17102eb710f5ddb6187c9e00e24
Debrena McEwen <debrenamcewen@gmail.com>
2017-01-04 15:53:32 -0800

    Change views app from pug to ejs and add getall function.

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
   id SERIAL PRIMARY KEY,
-  project_name VARCHAR(255),
+  project_name VARCHAR(255), NOT NULL,
   completed BOOLEAN DEFAULT false,
   rank INTEGER
 );
