6997bbc079e8b4b1bd6902bae54ad965155f470f
Debrena McEwen <debrenamcewen@gmail.com>
2017-01-04 10:53:36 -0800

    Creates a post method to the database and renders data to our homepage.

--- database/db.js
+++ database/db.js
@@ -1,6 +1,16 @@
 const config = require('config')
 const pgPromise = require('pg-promise')
 const pgp = pgPromise()
-const db = pgp(config.postgres)
+// const db = pgp(config.postgres)
+const db = pgp(`postgres://${process.env.USER}@localhost:5432/grandiose`)
 
-const createProjects =  'INSERT INTO projects (id, project_name) VALUES ($1, $2) RETURNING *'
+
+const createProjects =  'INSERT INTO projects (project_name) VALUES ($1) RETURNING *'
+
+const Projects = {
+  create: (project_name) => {
+    return db.oneOrNone( createProjects, [project_name] )
+  }
+}
+console.log('hello')
+module.exports = Projects
